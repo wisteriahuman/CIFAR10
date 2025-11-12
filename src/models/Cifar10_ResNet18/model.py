d2f0b639a0e5a3860e45d39e5b0ed8f57c687e15
wisteriahuman <wisteriahuman91@gmail.com>
2025-11-12 22:08:43 +0900

    commit

--- src/models/Cifar10_ResNet18/model.py
+++ src/models/Cifar10_ResNet18/model.py
@@ -4,6 +4,7 @@ from torchvision import models
 
 def get_cifar10_resnet18() -> models.ResNet:
     model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
     model.fc = nn.Linear(in_features=512, out_features=10)
 
     return model
