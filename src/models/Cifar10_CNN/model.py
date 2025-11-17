b68dcb8c99e224b4d39e98874b10ea357c5bfdb4
wisteriahuman <wisteriahuman91@gmail.com>
2025-11-18 04:41:27 +0900

    commit

--- src/models/Cifar10_CNN/model.py
+++ src/models/Cifar10_CNN/model.py
@@ -6,41 +6,37 @@ class Cifar10_CNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
-            nn.BatchNorm2d(num_features=32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
+            ResidualBlock(in_channels=3, out_channels=64),
+            ResidualBlock(in_channels=64, out_channels=64),
+            ResidualBlock(in_channels=64, out_channels=64),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(),
+            ResidualBlock(in_channels=64, out_channels=128),
+            ResidualBlock(in_channels=128, out_channels=128),
+            ResidualBlock(in_channels=128, out_channels=128),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
         )
         self.layer3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(),
+            ResidualBlock(in_channels=128, out_channels=256),
+            ResidualBlock(in_channels=256, out_channels=256),
+            ResidualBlock(in_channels=256, out_channels=256),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.1),
-        ) 
+        )
         self.layer4 = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=128 * 2 * 2, out_features=256),
-            nn.BatchNorm1d(num_features=256),
-            nn.ReLU(),
-            nn.Dropout(0.5),
+            ResidualBlock(in_channels=256, out_channels=512),
+            ResidualBlock(in_channels=512, out_channels=512),
+            ResidualBlock(in_channels=512, out_channels=512),
         )
         self.layer5 = nn.Sequential(
-            nn.Linear(in_features=256, out_features=128),
-            nn.BatchNorm1d(num_features=128),
+            nn.Flatten(),
+            nn.Linear(in_features=512 * 4 * 4, out_features=512),
+            nn.BatchNorm1d(num_features=512),
             nn.ReLU(),
             nn.Dropout(0.5),
         )
         self.layer6 = nn.Sequential(
-            nn.Linear(in_features=128, out_features=10),
+            nn.Linear(in_features=512, out_features=10),
         )
 
     def forward(self, x: TensorType["batch", 3, 32, 32]) -> TensorType["batch", 10]:
@@ -52,6 +48,32 @@ class Cifar10_CNN(nn.Module):
         x = self.layer6(x)
 
         return x
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+        )
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        identity = self.shortcut(x)
+        out = self.conv_block(x)
+        out += identity
+        out = self.relu(out)
+        return out
 
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
